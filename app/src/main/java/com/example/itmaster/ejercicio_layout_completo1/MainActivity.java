f63de7515b4c3660461ad19491f0586a2d595b63
LeonColombo <pabloleoncolombo@yahoo.com.ar>
2018-07-12 22:01:29 -0300

    MainActivity - findViewById
    creamos variables private en el main de tipo Button
    
    --Las vinculamos con findViewById R.id.peli1
    
    --usamos un peli1.setText para cambiarle el texto q tenia en el layout

--- app/src/main/java/com/example/itmaster/ejercicio_layout_completo1/MainActivity.java
+++ app/src/main/java/com/example/itmaster/ejercicio_layout_completo1/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.itmaster.ejercicio_layout_completo1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button peli1,peli2,peli3,peli4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        peli1 = findViewById(R.id.peli1);
+        peli2 = findViewById(R.id.peli2);
+        peli3 = findViewById(R.id.peli3);
+        peli4 = findViewById(R.id.peli4);
+
+
+        peli1.setText("Hello MF");
+        peli2.setText("DESCARGAR");
+        peli3.setText("WTF");
+        peli2.setText("F$$% YOU");
+        
+
+    }
+
+
+
+
     }
 }
